e855e8ed1fd9e5e0e1125eb56612fe29be20c790
Fequer Lima <fequerlima@hotmail.com>
2026-03-27 00:11:36 -0300

    Adicionar arquivos de projeto.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CrudMvcMySql.Models;
+
+namespace CrudMvcMySql.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options) 
+        {
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
---
+++ Models/Aluno.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudMvcMySql.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
